4d5c9dd017130da917dd063edebd744be35fd187
RodrigoSilva04 <2231653@my.ipleiria.pt>
2024-05-09 11:34:59 +0100

    criacao das bases do projeto todos os forms :

---
+++ Cantina/Cantina/Modelos/utilizador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina
+{
+    class utilizador
+    {
+        //Criar as 3 variaveis 
+        internal string Id;
+        internal string Nome;
+        internal string Nif;
+
+        public utilizador(string id,string nome,string nif)
+        {
+            //Atribuir os valores as variaveis
+            Nome = nome;
+            Id = id;
+            Nif = nif;
+        }
+    }
+}
